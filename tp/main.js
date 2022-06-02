f2461ab524a387e05e66efe1c7fcee459be37195
salmigl <85357848+salmigl@users.noreply.github.com>
2022-06-02 09:35:23 -0300

    Add files via upload

---
+++ tp/main.js
@@ -0,0 +1,17 @@
+
+
+// let vector=new Array("Babasonicos")
+// let canciones= ("Carismatico", "Bye Bye", "Y que?")
+// let vector2=["Radiohead"]
+let comidas= ["Paella", "Empanadas", "Tacos"]
+let paises=["España", "Argentina", "Mexico"]
+let platos=[paises,comidas]
+console.log (platos)  
+// agrega un elemento
+comidas.push("tostadas")
+console.log(comidas)
+// elimina el ultimo elemento
+comidas.pop()
+    console.log(comidas) 
+comidas.unshift()
+// console.log("hay " +canciones.length+ " canciones en la playlist")
